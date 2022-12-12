1254495c0a80f784f1bf89e2abd9922132e8df14
Christopher <Chris1Bobsy@Gmail.com>
2022-12-12 20:59:17 +0100

    vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,8 @@
+import * as path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+const projectRootDir = path.resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +10,8 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+  },
+  resolve: {
+    alias: [{ find: "src", replacement: path.resolve(projectRootDir, "src") }],
   },
 });
