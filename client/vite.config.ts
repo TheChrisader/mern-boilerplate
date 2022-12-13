297eae7811184b546011acbfc44094121a97c7a2
Christopher <Chris1Bobsy@Gmail.com>
2022-12-13 11:17:02 +0100

    run lint on save

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,24 @@
 import * as path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 const projectRootDir = path.resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    { ...eslint({ eslintPath: projectRootDir }), apply: "build" },
+    {
+      ...eslint({
+        failOnWarning: false,
+        failOnError: false,
+      }),
+      apply: "serve",
+      enforce: "post",
+    },
+  ],
   server: {
     port: 3000,
     open: true,
