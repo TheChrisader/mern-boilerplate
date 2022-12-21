4dcb2580f4c3159b4db977ab0d38516b21e90c12
Christopher <Chris1Bobsy@Gmail.com>
2022-12-21 18:53:08 +0100

    set up react-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+
 function App() {
-  return <div className="App">App</div>;
+  const { status, data, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => {
+      return fetch("https://jsonplaceholder.typicode.com/todos/1").then((res) =>
+        res.json()
+      );
+    },
+  });
+
+  if (status === "loading") {
+    return <span>Loading...</span>;
+  } else if (error instanceof Error && status === "error") {
+    return <span>Error: {error.message}</span>;
+  } else {
+    return <div className="App">Response: {JSON.stringify(data)}</div>;
+  }
 }
 
 export default App;
